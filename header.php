0aaf94d492127a9734a09a611c9ec04a2f61a445
Clark <chdotson14@gmail.com>
2023-04-16 14:03:05 -0400

    Completed Mailer

--- header.php
+++ header.php
@@ -11,8 +11,6 @@ $currentfile = basename($_SERVER['SCRIPT_FILENAME']);
 //Include Files
 require_once "connect.php";
 require_once "functions.php";
-
-
 ?>
 
 <!DOCTYPE html>
@@ -27,29 +25,30 @@ require_once "functions.php";
 <body>
 
 <header>
-    <img src="https://static.wixstatic.com/media/6af838_402426041bb236728860827d9b99bef3.png/v1/fill/w_181,h_199,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/6af838_402426041bb236728860827d9b99bef3.png"
-         align="left" alt="Logo" width="40" height="40">
-    <h1>Midway Fire Form</h1>
-    <nav><?php
+    <img class="headimg" src="media/midway_header.png" alt="Website Header Image" width="1024" height="203">
+
+    <nav>
+    <?php
+
         echo ($currentfile == "index.php") ? "Home" : "<a href='index.php'>Home</a>";
 
-
         //if the user is logged in display the following
-        if (isset($_SESSION['ID'])) {
+        //if (isset($_SESSION['ID'])) {
             echo ($currentfile == "formstart.php") ? "Forms" : "<a href='signup.php'>Forms</a>";
             echo ($currentfile == "search.php") ? "Search" : "<a href='search.php'>Search</a>";
+            echo ($currentfile == "email.php") ? "Email" : "<a href='email.php'>Email</a>";
 
 
             //if the user has admin status display the following
-            if ($_SESSION['status'] == 1) {
-                echo ($currentfile == "memberlist.php") ? "View Members" : "<a href='additems.php'>Add Cartoons</a>";
-            }
+           // if ($_SESSION['status'] == 1) {
+                echo ($currentfile == "memberlist.php") ? "View Members" : "<a href='additems.php'>Manage Employees</a>";
+           // }
             //if user is logged in, display log out
             echo ($currentfile == "logout.php") ? "Log Out" : "<a href='logout.php'>Log Out</a>";
             //else display log in
-        } else {
+       // } else {
             echo ($currentfile == "login.php") ? "Log In" : "<a href='login.php'>Log In</a>";
-        }
+       // }
         ?>
     </nav>
 </header>
--- index.php
+++ index.php
@@ -7,5 +7,18 @@ $currentfile = "index.php";
 ?>
 
 <p>
-    Lorem Ipsum
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ipsum magna, vestibulum eu velit vel, lobortis venenatis tellus. Donec efficitur nisl a diam sodales, quis dictum ante posuere. Sed gravida ullamcorper tortor ac tristique. Integer diam ante, dignissim nec bibendum ac, vehicula vel sapien. Aliquam ut tellus ac sapien scelerisque imperdiet in nec erat. Vestibulum sed molestie dui. Quisque vel fermentum nunc. Phasellus id tristique dui. Etiam sit amet sodales quam. Pellentesque leo libero, consequat non accumsan sed, dictum eu mi. Morbi vel lacinia mauris. Cras venenatis massa leo, et vehicula arcu viverra in. Vestibulum sagittis ipsum nec nisi consectetur eleifend. Nulla cursus ac libero sed bibendum. Nullam interdum imperdiet scelerisque. Sed gravida maximus quam, eget commodo nibh consectetur ac.
+
+    Suspendisse ipsum lacus, rhoncus quis dictum sed, suscipit ut velit. Ut quis sem ipsum. Nulla volutpat vehicula velit. Etiam luctus est vel leo ornare gravida. Aliquam dolor ipsum, vehicula id eros id, laoreet feugiat ex. Donec tempus vulputate purus vitae congue. Nunc semper urna nec tristique aliquet. Donec faucibus dapibus ultrices. Duis sodales, urna eu imperdiet auctor, est sem tempor enim, malesuada consequat quam mi in risus. Sed tristique gravida tristique. Integer non metus sed orci imperdiet finibus sit amet varius neque. Sed eget mi ornare sapien tincidunt porta. Maecenas tempor ipsum a lacus auctor laoreet.
+
+    Praesent faucibus molestie nibh finibus eleifend. Pellentesque vel est a arcu gravida euismod id nec risus. Phasellus ex tortor, sodales vel interdum vel, congue id felis. Morbi facilisis velit in purus sagittis posuere. Nunc auctor a metus consequat fermentum. Ut viverra tristique fermentum. Donec sem sapien, ultricies id neque sed, tempor egestas ex. Nulla ipsum urna, facilisis vehicula sem id, finibus consequat sem. Ut imperdiet odio ut velit aliquet fringilla. Fusce a fringilla metus. Quisque vel odio nibh. Nullam cursus ut nulla nec lacinia. Morbi vel finibus eros. Vestibulum imperdiet felis non pulvinar rhoncus. Morbi eget tempus nunc.
+
+    Phasellus dictum ut lacus at efficitur. Donec vulputate tristique ante. Donec non orci sit amet felis dictum blandit ut sed mauris. Mauris eros dui, ultricies et nunc ultrices, aliquam vehicula nulla. Ut quis dictum augue. Nunc sit amet lectus commodo, tincidunt massa sit amet, dapibus magna. Mauris vitae nisl vehicula, pretium tellus eu, ullamcorper velit. Nam et justo arcu. Nam mauris massa, consectetur ut nibh imperdiet, tempus congue urna. Nulla nec feugiat turpis. Sed vitae volutpat nibh, id rhoncus magna.
+
+    Pellentesque molestie finibus leo, at cursus ex vulputate at. Sed porttitor volutpat tortor, eget dignissim erat bibendum non. Aenean urna risus, mattis blandit finibus non, bibendum in ipsum. Aliquam at maximus nibh. Aliquam ac neque posuere, elementum nisl at, placerat urna. Sed rutrum ultricies nisl, ac maximus dolor. Quisque eget tellus eu quam venenatis accumsan. Vestibulum lobortis tempor mi quis malesuada. Praesent id justo aliquam, posuere erat sit amet, auctor dui. Quisque faucibus, metus ac dignissim tempus, risus ex auctor lectus, in convallis nisl sem eget sem. Suspendisse venenatis dictum libero eget imperdiet. Donec tempor iaculis nunc, nec ornare turpis posuere vel. Mauris tristique dui eget aliquet tincidunt. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed vulputate, sem bibendum aliquam iaculis, lorem arcu blandit tellus, a fermentum sem dolor feugiat elit.
 </p>
+
+
+<?php
+require_once "footer.php";
+?>
