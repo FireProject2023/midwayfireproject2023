368c46436688c9dba57f26dedbef046e0a209dd1
easexton <easexton@coastal.edu>
2023-05-03 12:17:15 -0400

    memberlist tested and functional, fixed styles

--- memberlist.php
+++ memberlist.php
@@ -3,6 +3,34 @@ $pagename = "Manage Employees";
 require_once "header.php";
 $currentfile = "memberlist.php";
 
+if ($_SERVER['REQUEST_METHOD'] == "POST"){
+    $approve = $_POST['approve'];
+    if ($approve==1) {
+        $sql = "UPDATE signup SET status = :status WHERE ID = :id";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(':status', $approve);
+        $stmt->bindValue(':id', $_POST['id']);
+        $stmt->execute();
+        //Success message
+        echo "<p class='success'>User has been approved.</p>";
+    } else if ($approve==0) {
+        $sql = "UPDATE signup SET status = :status WHERE ID = :id";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(':status', 0);
+        $stmt->bindValue(':id', $_POST['id']);
+        $stmt->execute();
+        //Success message
+        echo "<p class='success'>User access has been removed.</p>";
+    } else if ($approve==2) {
+        $sql = "UPDATE signup SET status = :status WHERE ID = :id";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(':status', 2);
+        $stmt->bindValue(':id', $_POST['id']);
+        $stmt->execute();
+        //Success message
+        echo "<p class='success'>User has been granted admin privileges.</p>";
+    }
+}
 //Sorting options
 if (isset($_GET['q'])) {
     switch ($_GET['q']){
@@ -21,17 +49,17 @@ if (isset($_GET['q'])) {
 }//isset q
 
 //query database
-$sql = "SELECT id, fname, lname, eml FROM signup ORDER BY $sorting";
+$sql = "SELECT * FROM signup ORDER BY $sorting";
 $stmt = $pdo->prepare($sql);
 $stmt->execute();
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-<div class="columnleft">
+
 <div class="section">
     <table id="members">
-        <tr><th>First Name <a href="<?php echo $currentfile; ?>?q=fa">&#x219F;</a> <a href="<?php echo $currentfile; ?>?q=fd">&#x21A1;</a>
-            </th><th>Email</th><th>Options</th></tr>
+        <tr><th>Name <a href="<?php echo $currentfile; ?>?q=fa">&#x219F;</a> <a href="<?php echo $currentfile; ?>?q=fd">&#x21A1;</a>
+            </th><th>Email</th><th>Options</th><th>Status</th><th>Admin Status</th></tr>
         <form name="approvemembers" id="approvemembers" method="post" action="<?php echo $currentfile; ?>">
         <?php
         //display to the screen
@@ -39,14 +67,39 @@ $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
         foreach ($result as $row){
             ?>
             <tr>
-                <td><?php echo $row['fname']; echo $row['lname'];?></td>
+                <td><?php echo $row['fname'] . " "; echo $row['lname'];?></td>
                 <td><?php echo $row['eml'];?></td>
                 <td>
                     <?php if ($_SESSION['ID'] == $row['id'] || $_SESSION['status'] == 2) {?>
-                        <a href="update.php?q=<?php echo $row['id'];?>">Update</a>
+                        <a href="update.php?q=<?php echo $row['id'];?>">Update</a><br>
                         <a href="changepwd.php?q=<?php echo $row['id'];?>">Change Password</a>
                     <?php } ?>
-
+                </td>
+                <td>
+                    <form name="approve" id="approve" action="<?php echo $currentfile; ?>" method="post">
+                    <?php if ($row['status'] == 0 && $_SESSION['status'] == 2) {?>
+                        <input type="hidden" name="id" value="<?php echo $row['ID']; ?>"/>
+                        <input type="hidden" name="approve" value="<?php echo 1; ?>"/>
+                        <input type="submit" name="submit" id="submit" value="Approve">
+                    <?php } else if ($row['status'] != 0 && $_SESSION['status'] == 2) {?>
+                        <input type="hidden" name="id" value="<?php echo $row['ID']; ?>"/>
+                        <input type="hidden" name="approve" value="<?php echo 0; ?>"/>
+                        <input type="submit" name="submit" id="submit" value="Remove">
+                    <?php } ?>
+                    </form>
+                </td>
+                <td>
+                    <form name="admin" id="admin" action="<?php echo $currentfile; ?>" method="post">
+                    <?php if ($row['status'] != 2 && $_SESSION['status'] == 2) { ?>
+                        <input type="hidden" name="id" value="<?php echo $row['ID']; ?>"/>
+                        <input type="hidden" name="approve" value="<?php echo 2; ?>"/>
+                        <input type="submit" name="submit" id="submit" value="Make Admin">
+                    <?php } else if ($row['status'] == 2 && $_SESSION['status'] == 2) {
+                        $approve=1; ?>
+                        <input type="hidden" name="id" value="<?php echo $row['ID']; ?>"/>
+                        <input type="submit" name="submit" id="submit" value="Remove Admin">
+                    <?php } ?>
+                    </form>
                 </td>
             </tr>
             <?php
@@ -55,13 +108,8 @@ $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
         </form>
     </table>
-</div></div>
-
-<div class="columnright">
-    <div class="section">
-        <h2>Insert info here! Maybe: # of inspections completed in year, weather?</h2>
-    </div>
 </div>
+
 <?php
 require_once "footer.php";
 ?>
--- update.php
+++ update.php
@@ -1,1 +1,129 @@
 <?php
+
+$pagename = "Member Update";
+require_once "header.php";
+$currentfile = "update.php";
+
+//check where user came from
+if (isset($_SERVER['HTTP_REFERER'])) {
+    $refer = basename($_SERVER['HTTP_REFERER']);
+} else {
+    $refer ="";
+}
+
+//get id
+if ($_SERVER['REQUEST_METHOD'] == "GET" && isset($_GET['q'])){
+    $ID = $_GET['q'];
+} else if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['ID'])) {
+    $ID = $_POST['ID'];
+    $refer = "list.php";
+} else {
+    echo "<p class='error'>An error occurred. Please navigate back to the home page and try again.</p>";
+    $errexists = 1;
+}
+
+//make sure user came from the Member List
+if ($refer != "list.php")  {
+    echo "<p class='error'>You cannot access this page directly.</p>";
+    ?>
+    <a href="memberlist.php">Return to Member List</a>
+    <?php
+} else {
+
+    //initial variables
+    $showform = 1;
+    $errexists = 0;
+    $erreml = "";
+    $errfname = "";
+
+    //form processing
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        //local variables & sanitization
+        $fname = trim($_POST['fname']);
+        $eml = trim(strtolower($_POST['eml']));
+
+        //error checking
+        if (empty($fname)) {
+            $errexists = 1;
+            $errfname = "Preferred name was left empty.";
+        }
+
+        if (empty($eml)) {
+            $errexists = 1;
+            $erreml = "Email was left empty.";
+        } else {
+            //check email
+            if (!filter_var($eml, FILTER_VALIDATE_EMAIL)) {
+                $errexists = 1;
+                $erreml = "Email is not valid";
+            } else if ($eml != $_POST['origeml']) {
+                $sql = "SELECT * FROM signup WHERE eml = ?";
+                $emailexists = checkdup($pdo, $sql, $eml);
+                if ($emailexists) {
+                    $errexists = 1;
+                    $erreml = "The email is taken.";
+                }
+            }
+        }
+
+        //general error message
+        if ($errexists == 1) {
+            echo "<p class='error'>There are errors. Please make changes and resubmit.</p>";
+        } else {
+            //no errors, update database
+            //updated date
+            $updated = date("Y-m-d H:i:s");
+
+            //update database
+            $sql = "UPDATE signup SET fname = :fname, eml = :eml, updated = :updated WHERE ID = :ID";
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindValue(':fname', $fname);
+            $stmt->bindValue(':eml', $eml);
+            $stmt->bindValue(':updated', $updated);
+            $stmt->bindValue(':ID', $ID);
+            $stmt->execute();
+            //Success message
+            echo "<p class='success'>You have successfully updated your information.</p>";
+
+            //hide form
+            $showform = 0;
+
+        }//else control code
+    }//submit
+    if ($showform == 1) {
+        $sql = "SELECT * from signup WHERE ID = :ID";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(':ID', $ID);
+        $stmt->execute();
+        $row = $stmt->fetch();
+        ?>
+
+        <p>Please enter your new information, or leave anything you do not want to change the same. All fields are required.</p>
+
+        <form name="update" id="update" action="<?php echo $currentfile; ?>" method="post">
+            <label for="fname">New Name:</label>
+            <input type="text" name="fname" id="fname" placeholder="Preferred Name" maxlength="50" value ="<?php if (isset($fname) && (!empty($fname))) {
+                echo htmlspecialchars($fname, ENT_QUOTES, "UTF-8"); } else { echo htmlspecialchars($row['fname'], ENT_QUOTES, "UTF-8");}?>">
+            <?php if (!empty($errfname)) {echo "<span class ='error'>$errfname</span>"; } ?>
+            <br>
+            <br>
+            <label for="eml">New Email:</label>
+            <input type="email" name="eml" id="eml" placeholder="Email Address" maxlength="255" value ="<?php if (isset($eml) && (!empty($eml))) {
+                echo htmlspecialchars($eml, ENT_QUOTES, "UTF-8"); } else { echo htmlspecialchars($row['eml'], ENT_QUOTES, "UTF-8");}?>">
+            <?php if (!empty($erreml)) {echo "<span class ='error'>$erreml</span>"; } ?>
+            <br>
+
+            <input type="hidden" name="ID" value="<?php echo $row['ID'];?>">
+            <input type="hidden" name="origname" value="<?php echo $row['fname'];?>">
+            <input type="hidden" name="origeml" value="<?php echo $row['eml'];?>">
+
+            <br><label for="submit">Submit:</label>
+            <input type="submit" name="submit" id="submit" value="submit">
+            <br>
+        </form>
+
+        <?php
+    }//close showform
+}//close refer
+require_once "footer.php";
+?>
